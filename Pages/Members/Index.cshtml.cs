e8d80a6be4d68f27172e05825003196f15f1cc0b
Matt Venter <mventer@hawk.iit.edu>
2020-11-23 07:42:24 -0600

    Part 3 is finsihed except for grouping as we don't thave the proper stuff to even use it yet.

--- Pages/Members/Index.cshtml.cs
+++ Pages/Members/Index.cshtml.cs
@@ -22,16 +22,31 @@ namespace Group22_ParkingApp.Pages.Members
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
         public IList<Member> Member { get;set; }
+        public PaginatedList<Member> Members { get; set; }
 
-        public async Task OnGetAsync(string sortOrder)
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            Member = await _context.Members.ToListAsync();
+            CurrentSort = sortOrder;
             // using System;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
 
             IQueryable<Member> membersIQ = from m in _context.Members
                                              select m;
-
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                membersIQ = membersIQ.Where(m => m.LastName.Contains(searchString)
+                                       || m.FirstName.Contains(searchString));
+            }
             switch (sortOrder)
             {
                 case "name_desc":
@@ -43,7 +58,9 @@ namespace Group22_ParkingApp.Pages.Members
                     break;
             }
 
-            Member = await membersIQ.AsNoTracking().ToListAsync();
+            int pageSize = 3;
+            Members = await PaginatedList<Member>.CreateAsync(
+                membersIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 
